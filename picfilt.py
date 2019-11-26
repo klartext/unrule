3e25ce880a6ad8b73a2166c3ec8c7b9c54d04d68
Oliver Bandel <oliver@first.in-berlin.de>
2019-11-26 01:42:32 +0100

    Bearbeiten und speichern geht schon mal.

--- picfilt.py
+++ picfilt.py
@@ -35,4 +35,40 @@ def save_nparray_as_pic(nparr, filename):
 
 bwpicarr = readimage_as_grayval_to_array("lemma.png")
 
-save_nparray_as_pic(bwpicarr, "out.png")
+print(type(bwpicarr))
+print(bwpicarr.ndim)
+print(bwpicarr.shape)
+print(bwpicarr.dtype)
+print(bwpicarr.size)
+
+ins = 4
+stretch    = 1
+outs = 2 * stretch + ins
+
+outpicarray = bwpicarr.copy()
+
+for xval in range(50,600):
+    for yval in range(40,200):
+        outer = bwpicarr[yval : yval + outs,   xval : xval + outs]
+        inner = outer[stretch : ins + stretch, stretch : ins + stretch]
+        print("outer shape:", outer.shape)
+        print("inner shape:", inner.shape)
+
+        outersum = outer.sum()
+        innersum = inner.sum()
+        framesum = outersum - innersum
+
+        frameav = framesum / (outs*outs - ins*ins)
+        innerav = innersum / (ins*ins)
+
+        #print("frameav | innerav | diff", frameav, innerav, frameav - innerav)
+        #arithmittel = outer.sum()/outer.size
+
+        #print(outer)
+        #print("Mittelwert:", arithmittel)
+        for yo in range(0,outs):
+            for xo in range(0,outs):
+                outpicarray[yval+xo][xval+xo] = frameav
+
+
+save_nparray_as_pic(outpicarray, "out.png")
