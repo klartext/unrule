22cf28c2c58af9e6449c295a45db6644f7038dc7
Oliver Bandel <oliver@first.in-berlin.de>
2019-11-26 22:23:20 +0100

    Weitere Versuche, das ergebnis zu verbessern (hor+ver) haben nichts gebracht.
    Genug für heute.
    Muss ich nochmal drüber nachdenken (Algorithmus / Python-Code).

--- picfilt.py
+++ picfilt.py
@@ -5,25 +5,63 @@ import numpy as np
 from PIL import Image
 
 class Antikaro:
-    def __init__(self, bwpicarray, ins, stretch):
+    def __init__(self, filename):
+
+        self.filename = filename
+        self.bwpa = readimage_as_grayval_to_array(filename)
+        bwpicarray = self.bwpa
         #self.bwpicarray = bwpicarr
-        self.height, self.width = bwpicarr.shape
+        self.height, self.width = bwpicarray.shape
 
-        self.ins     = ins
-        self.stretch = stretch
-        self.outs    = 2 * stretch + ins
+        # set defaults
+        self.ins = 1
+        self.stretch = 2
+        self.calc_outs()
 
-        outpicarray = self.bar(bwpicarray)
+        #self.outpicarray = self.remove_lineature()
         if False:
             outpicarray = np.transpose(outpicarray)
-            outpicarray = self.bar(outpicarray)
+            outpicarray = self.remove_lineature(outpicarray)
             outpicarray = np.transpose(outpicarray)
-        save_nparray_as_pic(outpicarray, "out.png")
 
 
-    def bar(self, bwpicarray):
+    def calc_outs(self):
+        self.outs = 2 * self.stretch + self.ins
+
+    def set_stretch(self, stretch):
+        self.stretch = stretch
+        self.calc_outs()
+
+    def set_ins(self, ins):
+        self.ins = ins
+        self.calc_outs()
+
+    def set_avdiff_lower(self, lowval):
+        self.avdiff_lower = lowval
+
+    def set_avdiff_higher(self, highval):
+        self.avdiff_higher = highval
+
+    def save(self, outfilename):
+        save_nparray_as_pic(self.outpicarray, outfilename)
+
+    def save2(self, outfilename):
+        if not hasattr(self, outpicarray):
+            print("No output array was created. Using the original picture")
+            save_nparray_as_pic(self.bwpa, outfilename)
+        else:
+            save_nparray_as_pic(self.outpicarray, outfilename)
+
+    def transpose(self):
+        self.bwpa = np.transpose(self.bwpa)
+        self.outpicarray = np.transpose(self.outpicarray)
+
+
+
+    def remove_lineature(self):
     #ins, outs, width, height, image_array ):
 
+        bwpicarray = self.bwpa
         height, width = bwpicarray.shape
 
         outs = self.outs
@@ -39,8 +77,8 @@ class Antikaro:
                 left = bwpicarray[yval,  xval - outs : xval - 1]
                 right = bwpicarray[yval,  xval +1 : xval + outs]
 
-                lav = left.sum()/stretch
-                rav = right.sum()/stretch
+                lav = left.sum()/self.stretch
+                rav = right.sum()/self.stretch
 
                 avdiff = abs(lav - rav)
 
@@ -51,7 +89,30 @@ class Antikaro:
                 else:
                     outpicarray[yval][xval] = bwpicarray[yval][xval]
 
+        #self.outpicarray = outpicarray
+        #return outpicarray
+
+        for yval in range(outs, height - outs):
+            for xval in range(outs, width - outs):
+
+                above = bwpicarray[yval - outs : yval - 1, xval]
+                below = bwpicarray[yval +1 : yval + outs, xval]
+
+                aav = above.sum()/self.stretch
+                bav = below.sum()/self.stretch
+
+                avdiff = abs(aav - bav)
+
+                # Standardabweichung noch checken -> wenn zu groß, dann nicht verändern
+
+                if avdiff > avdiff_low and avdiff < avdiff_high:
+                    outpicarray[yval][xval] = (aav + bav) / 2
+                else:
+                    outpicarray[yval][xval] = bwpicarray[yval][xval]
+
+        self.outpicarray = outpicarray
         return outpicarray
+
 
 
 def readimage_to_array(filename):
@@ -62,13 +123,6 @@ def readimage_as_grayval_to_array(filename):
     pic = Image.open(filename)
     pic_grayval = pic.convert('L') # Stimmt "L" überhaupt?
     return np.array(pic_grayval)
-
-
-
-
-# Wie dann wider Bild aus dem Array machen?
-# picBW.putdata(picarr) # zurück schreiben
-# Image.fromarray(pix) # neues Bild
 
 def save_nparray_as_pic(nparr, filename):
     newimage = Image.fromarray(nparr)
@@ -81,29 +135,10 @@ def save_nparray_as_pic(nparr, filename):
 
 
 
-
-
-
-
-
-#print(type(bwpicarr))
-#print(bwpicarr.ndim)
-#print(bwpicarr.shape)
-#print(bwpicarr.dtype)
-#print(bwpicarr.size)
-
-#print("width:", width)
-#print("height:", height)
-
-
-#print("ins =", ins)
-#print("outs =", outs)
-#print("stretch =", stretch)
-#print("height:", height)
-
-# plt.imshow(pic)
-
-ins = 1
-stretch  = 2
-bwpicarr = readimage_as_grayval_to_array("lemma.png")
-foo = Antikaro(bwpicarr, ins, stretch)
+foo = Antikaro("lemma.png")
+foo.set_ins(1)
+foo.set_stretch(2)
+#foo.transpose()
+foo.remove_lineature()
+#foo.transpose()
+foo.save("out.png")
