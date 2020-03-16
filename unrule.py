b5df127114e5885e0248fd27270cb9af7180089b
Oliver Bandel <oliver@first.in-berlin.de>
2020-03-16 01:23:34 +0100

    Loops replaced by vectorizes stuff in linedata-patching.
    Amazing speedup.

--- unrule.py
+++ unrule.py
@@ -17,7 +17,8 @@ def pdl(data, comment):
     """
     print data and length with comment
     """
-    print("{:8s}:  (len = {:4d}}, data = {}".format(comment, len(data), data))
+    print("length of {:12s}= {}".format(comment, len(data)))
+    print("{:10s}{:12s}= {}".format("", comment, data))
 
 
 def moving_average(array, avlen):
@@ -44,13 +45,13 @@ def moving_average_with_diff(array, stretch, inner_len):
 
     conv_avdiff = np.concatenate((leftconv, inbetween, rightconv))
     diff = np.convolve(array, conv_avdiff, 'valid') * (-1) # (-1) because left-right was used for avdiff
-    print("conv_avdiff:", conv_avdiff)
+    #print("conv_avdiff:", conv_avdiff)
 
     # mean-conv
     meanconv  = np.repeat(1/(2*stretch), stretch)
     conv_mean = np.concatenate((meanconv, inbetween, meanconv))
     mean = np.convolve(array, conv_mean, 'valid')
-    print("conv_mean:  ", conv_mean)
+    #print("conv_mean:  ", conv_mean)
 
     return diff, mean
 
@@ -120,6 +121,40 @@ class Antikaro:
 
 
 
+    def patch_linedata(self, linedata):
+        # for lazy typing
+        ins     = self.ins     # length inner part
+        stretch = self.stretch # "stretching" each side of inner by stretch
+
+        dlen = len(linedata)
+
+        mvaver    = moving_average(linedata, stretch) # kann spaeter weg
+        innermvav = moving_average(linedata[stretch : -stretch], ins)     # kann spaeter weg
+
+        averdiff_arr, newval_arr = moving_average_with_diff(linedata, stretch, ins)
+
+        if False: # need to think about this rounding again
+            mvaver        +=  0.5
+            innermvav     +=  0.5
+            averdiff_arr  +=  0.5
+            newval_arr    +=  0.5
+
+        subwin = linedata[stretch : -stretch]
+
+        # find the indexes where changes need to be done
+        # (hard coded values, I know it's bad - remove if parametrisation makes sense)
+        wo = np.where((averdiff_arr < 10) & (averdiff_arr > -10) &
+                      (innermvav - newval_arr > -50) & (innermvav - newval_arr < -1))
+
+        # set new values for all ins entries
+        for offset in range(ins):
+            bidxa = (offset + wo[0],)
+            subwin[bidxa] = newval_arr[wo] # Set the new values
+
+        return linedata
+
+
+
     # Here the main work will be done
     # ===============================
     def remove_lineature(self):
@@ -127,14 +162,12 @@ class Antikaro:
         bwpicarray = self.bwpa
         height, width = bwpicarray.shape
 
+        # for lazy typing
         ins     = self.ins     # length inner part
         stretch = self.stretch # "stretching" each side of inner by stretch
         outs    = self.outs    # whole length of inner + 2 * stretched
 
         outpicarray = bwpicarray.copy()
-
-        print("SHAPE OF outpicarray:", outpicarray.shape)
-        print("ELEMENT OF outpicarray:", outpicarray[0][0])
 
         avdiff_low = -20
         avdiff_high = 0
@@ -148,135 +181,22 @@ class Antikaro:
         t0 = pc()
         for yval in range(0, height - outs):
 
-            print("----------- Zeilenindex: {} ----------------------------".format(yval))
             linedata      = outpicarray[yval]
-            dlen = len(linedata)
-
-            # ----------------------------------------------------->>>
-            linedata      = np.array([10,10,20,40,10,0,
-                                  12,10,19,43,10,2,
-                                  10,33,2,4,22,
-                                 ])# TESTING
-            # <<<-----------------------------------------------------
-            mvaver    = moving_average(linedata, self.stretch) # kann spaeter weg
-            innermvav = moving_average(linedata, self.ins)     # kann spaeter weg
-
-            #pdl(linedata, "linedata")
-
-            averdiff_arr, newval_arr = moving_average_with_diff(linedata, self.stretch, self.ins)
-
-            #print("height, width =  {} * {}".format(height, width))
-            #print("datalen: {}, mvaver-len: {}".format(len(linedata), len(mvaver)))
-
-            print("linedata-length:   ", len(linedata))
-            print("linedata:          ", linedata)
-            print("mvaver-length  ", len(mvaver))
-            print("mvaver:        ", mvaver)
-            print()
-            print("averdiff-len:", len(averdiff_arr))
-            print("averdiff_arr:", averdiff_arr)
-            print("newval-len:  ", len(newval_arr))
-            print("newval_arr:  ", newval_arr)
-
-            print("averdiff_arr.shape:", averdiff_arr.shape)
-            print("newval_arr.shape:  ", newval_arr.shape)
-
-            #print("len(innermvav) = {}, len(newval_arr) = {} \n".format( len(innermvav), len(newval_arr)), flush=True)
-            thisline = linedata[yval,]
-
-            subwin = linedata[stretch: -stretch]
-            print("subwin:            ", subwin)
-            # ISSUE WITH THE INDEXING
-            #wo = np.where((averdiff_arr < 10) & (averdiff_arr > -10) &
-            #              (innermvav - newval_arr > -50) & (innermvav - newval_arr < -1))
-            #wo = np.where((averdiff_arr < 10) & (averdiff_arr > -10) )
-            #wo = np.where((averdiff_arr > -30) & (averdiff_arr < -20) ) # TESTING with versmall.jpg
-            wo = np.where( (averdiff_arr > 5) & (averdiff_arr < 10) ) # TESTING
-            print("type(wo):          ", type(wo))
-            print("wo[0].shape:       ", wo[0].shape)
-            print("type(wo[0])        ", type(wo[0]))
-            print("*** linedata.shape:", linedata.shape)
-            print("*** linedata[wo].shape", linedata[wo].shape)
-            print("WHERE-Indizes wo:  ", wo)
-            print("linedata:          ", linedata)
-            print("linedata[wo]       ", linedata[wo])
-
-            preselect = ins
-            print("linedata[sub][wo]       ", linedata[preselect:,][wo])
-
-            #print("type of wo:", type(wo))
-            #print("        wo:", wo)
-            #newshape = linedata[self.stretch + self.ins - 1 : 2* self.stretch + self.ins -1]
-            #print("*** newshape:", newshape)
-            linedata[preselect:,][wo] = newval_arr[wo]
-            print("linedata (changed):", linedata)
-            #outpicarray[yval] = linedata
-            print("--------------------------------------------------------".format(yval))
-            exit(99)# TESTING
-            continue
-
-
-            for xval in range(0, width - outs):
-                lav   = mvaver[xval]
-                insav = innermvav[xval+stretch]
-                rav   = mvaver[xval+stretch+ins]
-
-                avdiff = (lav - rav)      # for decision if newval is used
-                newval =  (lav + rav) / 2 # the new value for inside, if used at all
-
-                print("*** Loop: avdiff / newval: {} / {}".format(avdiff, newval))
-
-                # Standardabweichung noch checken -> wenn zu groß, dann nicht verändern
-#(yval, xval) = ( 66,  59  -> avdiff = 65.666667, insav - newval: 32.833333)
-#(yval, xval) = ( 66,  60  -> avdiff = 75.000000, insav - newval: 21.833333)
-#(yval, xval) = ( 66,  61  -> avdiff = 75.000000, insav - newval: -3.166667)
-#(yval, xval) = ( 66,  62  -> avdiff = 75.000000, insav - newval: -28.166667)
-#
-                #print("(yval, xval) = ({0:3d}, {1:3d}  -> avdiff = {2:f}, insav - newval: {3:f})".format(yval, xval, avdiff, insav - newval) )
-                if value_in_interval(avdiff, [-10,10]) and  value_in_interval(insav - newval, [-50, -1]):
-                #    print("(yval, xval) = ({0:3d}, {1:3d}  -> avdiff = {2:f}, insav - newval: {3:f})".format(yval, xval, avdiff, insav - newval) )
-                    for idx in range(stretch + 1, stretch + ins + 1):
-                        xpos = xval + idx - int(ins/2) # ins/2 abziehen, weil nicht nur nach vorne abziehen (???)
-
-                        #newval = 0
-                        outpicarray[yval][xpos] = newval
-
-            # Alternative way: via Array-conditions
+            linedata = self.patch_linedata(linedata)
+            outpicarray[yval] = linedata
 
         t1 = pc()
-        print("# HORIZONTAL: {:8.3f}".format(t1 - t0), file=sys.stderr, flush=True)
+        #print("# HORIZONTAL: {:8.3f}".format(t1 - t0), file=sys.stderr, flush=True)
 
         # VERTIKAL
         for xval in range(0, width - outs):
 
-            # welche Methode ist schneller?
-            #data = np.transpose(outpicarray)[xval] # funktioniert auch
-            data = outpicarray[:,xval] # funktioniert
-
-            mvaver      = moving_average(data, self.stretch)
-            innermvaver = moving_average(data, self.ins)
-
-
-            for yval in range(0, height - outs):
-                aav   = mvaver[yval]
-                insav = innermvaver[yval + stretch]
-                bav   = mvaver[yval + stretch + ins ]
-
-                avdiff = (aav - bav)      # for decision if newval is used
-                newval =  (aav + bav) / 2 # the new value for inside, if used at all
-
-                # Standardabweichung noch checken -> wenn zu groß, dann nicht verändern
-
-                #print("(yval, xval) = ({0:3d}, {1:3d}  -> avdiff = {2:f}, insav - newval: {3:f})".format(yval, xval, avdiff, insav - newval) )
-                if value_in_interval(avdiff, [-10,10]) and  value_in_interval(insav - newval, [-50, -1]):
-                    for idx in range(stretch + 1, stretch + ins + 1):
-                        ypos = yval + idx - int(ins/2) # ins/2 abziehen, weil nicht nur nach vorne abziehen (???)
-
-                        #newval = 0
-                        outpicarray[ypos][xval] = newval
+            linedata      = outpicarray[:,xval]
+            linedata = self.patch_linedata(linedata)
+            outpicarray[:,xval] = linedata
 
         t2 = pc()
-        print("# VERTIKAL: {:8.3f}".format(t2 - t1), file=sys.stderr, flush=True)
+        #print("# VERTIKAL: {:8.3f}".format(t2 - t1), file=sys.stderr, flush=True)
 
         #print("===============================================")
         self.outpicarray = outpicarray
